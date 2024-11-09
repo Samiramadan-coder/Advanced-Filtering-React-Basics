69a1def3ccec5cd2a8b1086982ff93b9020e7182
Sami Ramadan <samyramadan.bts@gmail.com>
2024-11-09 12:43:39 +0200

    price

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -1,7 +1,34 @@
 import "./Category.css";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+
+      <div className="sidebar-items">
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandels
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heals
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
